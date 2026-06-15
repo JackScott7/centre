c4f6ba9ea029cb3284fc50276c72120296014483
JackScott7 <cloner.bl12@gmail.com>
2026-06-15 11:50:25 +0330

    -v --version arg implemented into cli

--- src/centre/cli.py
+++ src/centre/cli.py
@@ -7,10 +7,12 @@ def main() -> None:
     parser = argparse.ArgumentParser(prog="centre", description='Centre is A lightweight Windows tool'
                                     ' for consistent app placement, sizing, and desktop layout control.')
 
+    parser.add_argument('-s', '--start', action='store_true', help='Start Centre as background process')
+
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-l', '--list', action='store_true', help='List Window Title, Size and Position')
-    group.add_argument('-s', '--start', action='store_true', help='Start Centre as background process')
     group.add_argument('-c', '--read-config', action='store_true', help='Read config file and print out')
+    group.add_argument('-v', '--version', action='version', version=f'centre {Utilities.get_package_version()}')
 
     args = parser.parse_args()
 
@@ -26,6 +28,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-
-__all__ = ["Centre"]
