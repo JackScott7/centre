462a2f3f5e9e865ea6fd0632ce7c01c089183fdb
Jack Scott <cloner.bl12@gmail.com>
2026-07-27 17:05:29 +0330

    feat: implemented logging | fix: consistent var names. better exception handling

--- src/centre/utilities.py
+++ src/centre/utilities.py
@@ -3,10 +3,14 @@ import win32process
 import psutil
 import os
 import pygetwindow as gw
+import logging
 from pyautogui import size as resolution_size
 from importlib.metadata import version, PackageNotFoundError
 from pygetwindow import Win32Window
 from .schemas import Config, WindowPreset, ActiveWindow
+
+
+log = logging.getLogger("centre")
 
 
 class Utilities:
@@ -21,8 +25,8 @@ class Utilities:
 
     @staticmethod
     def get_log_file_path() -> str:
-        config_dir = os.path.join(os.path.expanduser("~"), '.centre')
-        return os.path.join(config_dir, 'centre.log')
+        log_dir = os.path.join(os.path.expanduser("~"), '.centre')
+        return os.path.join(log_dir, 'centre.log')
 
     @staticmethod
     def get_config_file_path() -> str:
@@ -31,9 +35,14 @@ class Utilities:
 
     @staticmethod
     def minimize_window_hotkey() -> None:
-        window = gw.getActiveWindow()
-        if window:
-            window.minimize()
+        window, name = Utilities.get_window()
+
+        if not window or not name:
+            log.info("No Active window found on minimize")
+            return
+
+        log.info("Minimizing %s", name)
+        window.minimize()
 
     @staticmethod
     def center_hotkey(centre) -> None:
@@ -41,6 +50,7 @@ class Utilities:
 
         window, window_name = Utilities.get_window()
         if not window or not window_name:
+            log.info("No active window was found on center")
             return
 
         ignore_list = config.ignored_presets
@@ -50,13 +60,16 @@ class Utilities:
         app_poses = config.presets[Utilities.get_display_resolution()]
 
         if window_name in app_poses.keys():
+            log.info(f"Centering %s", window_name)
             window.resizeTo(app_poses[window_name].SIZE_X, app_poses[window_name].SIZE_Y)
             window.moveTo(app_poses[window_name].LEFT, app_poses[window_name].TOP)
             return
 
         if not app_poses.get('Default_Position'):
+            log.info("No Default Position is defined")
             return
 
+        log.info(f"Centering %s to Default_Position", window_name)
         window.resizeTo(app_poses['Default_Position'].SIZE_X, app_poses['Default_Position'].SIZE_Y)
         window.moveTo(app_poses['Default_Position'].LEFT, app_poses['Default_Position'].TOP)
 
@@ -94,6 +107,7 @@ class Utilities:
     def capture_hotkey(centre) -> None:
         window, window_name = Utilities.get_window()
         if not window or not window_name:
+            log.info("No active window was found on capture")
             return
 
         preset: WindowPreset = WindowPreset(
@@ -102,6 +116,8 @@ class Utilities:
             SIZE_X=window.size.width,
             SIZE_Y=window.size.height
         )
+
+        log.info(f"New/Update Preset (%s):\n%s", window_name, preset)
 
         Utilities.add_update_preset(window_name, preset, centre)
 
@@ -117,6 +133,7 @@ class Utilities:
     def ignore_window_hotkey(centre) -> None:
         window, window_name = Utilities.get_window()
         if not window or not window_name:
+            log.info("No active window was found on window ignore")
             return
 
         config: Config = centre.config
@@ -125,6 +142,7 @@ class Utilities:
 
         if window_name not in ignored_presets:
             ignored_presets.append(window_name)
+            log.info(f"Ignoring %s", window_name)
             Utilities.update_config(config)
 
     @staticmethod
@@ -137,10 +155,10 @@ class Utilities:
     @staticmethod
     def center_all_hotkey(centre) -> None:
         windows: list[ActiveWindow] = []
-
         for window in gw.getAllWindows():
             _, window_name = Utilities.get_window(window)
             if not window_name:
+                log.info("No active window was found on center all")
                 continue
 
             windows.append({
@@ -149,18 +167,23 @@ class Utilities:
             })
 
         presets = centre.config.presets[Utilities.get_display_resolution()]
+
+        window_names = [x["name"] for x in windows]
+
         if not presets:
+            log.info(f"No presets found, windows:\n%s", window_names)
             return
 
         all_active = [window for window in windows if window["name"] in presets.keys()]
 
         if not all_active:
+            log.info(f"No matching presets found, windows:\n%s", window_names)
             return
 
         for window in all_active:
             win = window["win"]
             name = window["name"]
-
+            log.info(f"Centering %s", name)
             win.resizeTo(presets[name].SIZE_X, presets[name].SIZE_Y)
             win.moveTo(presets[name].LEFT, presets[name].TOP)
 
@@ -175,12 +198,21 @@ class Utilities:
 
         :param window: Your specific window or None (to get the active window)
         """
-        if not window:
+        if window is None:
             window = gw.getActiveWindow()
+
+        if window is None:
+            return None, None
 
         try:
             _, pid = win32process.GetWindowThreadProcessId(window._hWnd)
             process_name = os.path.splitext(psutil.Process(pid).name())[0].upper()
             return window, process_name
-        except (IndexError, Exception):
+        except psutil.NoSuchProcess as e:
+            log.exception("No Such Process exists. %s", e)
+            return None, None
+        except psutil.AccessDenied as e:
+            log.exception("Access Denied. %s", e)
+            return None, None
+        except Exception:
             return None, None
