a0300c24c8cbe491fc742955b491dccc27e47b87
Jack Scott <cloner.bl12@gmail.com>
2026-07-27 17:09:12 +0330

    feat: raising SystemExit will start the shutdown procedure, keyboard and FileObserver hooks will be disposed before exiting interpreter

--- src/centre/configwatcher.py
+++ src/centre/configwatcher.py
@@ -10,5 +10,8 @@ class ConfigWatcher(FileSystemEventHandler):
     def on_modified(self, event: FileModifiedEvent):
         event_path = Path(str(event.src_path)).resolve()
 
-        if event_path == self.config_path:
-            self.centre.load_config()
+        try:
+            if event_path == self.config_path:
+                self.centre.load_config()
+        except SystemExit as ex:
+            self.centre.request_shutdown(str(ex) or "Centre stopped because the configuration is invalid.")
